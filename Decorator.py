d2993d41d882892d6765cbb2e9e40e6943ed54b3
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-29 01:52:50 +0400

    Add files via upload

--- Decorator.py
+++ Decorator.py
@@ -1,38 +1,100 @@
-#Decorator
+"""
+Decorator design pattern:
+It adds new behaviour without changing its original class,
+by wrapping it into another object.
+"""
 
 class Coffee:
+    """
+    Base Coffee class, for basic black coffee
+    """
     def price(self):
+        """
+        Return basic coffee's price
+
+        Return:
+            float: basic black coffee price
+        """
         return 5
     
-class CoffeeDecorator:
+
+class CoffeeDecorator(Coffee):
+    """
+    Base class for coffee decorators
+
+    Attribute:
+        coffee (string): coffee type
+    """
     def __init__(self, coffee):
+        """
+        Add behaviour around coffee object
+
+        Args:
+            coffee (string): coffee type) 
+        """
         self._coffee = coffee
 
+
     def price(self):
+        """
+        Returns coffee price
+
+        Return:
+            float: coffee object price
+        """
         return self._coffee.price()
 
 
-class Cappuccino(CoffeeDecorator):
+class MilkDecorator(CoffeeDecorator):
+    """
+    Adds milk to the coffee
+    """
     def __init__(self, coffee):
+        """
+        Initialize milk decorator with a coffee instance
+
+        Args:
+            coffee (string): coffee type
+        """
         self._coffee = coffee
 
+    
     def price(self):
+        """
+        calculate total price of the coffee, after adding milk
+        """
         return self._coffee.price() + 1.5
 
-class Espresso(CoffeeDecorator):
+
+class SugarDecorator(CoffeeDecorator):
+    """
+    Adds sugar to the coffee
+    """
     def __init__(self, coffee):
+        """
+        Initialize sugar decorator with a coffee instance
+
+        Args:
+            coffee (string): coffee type
+        """
         self._coffee = coffee
 
     def price(self):
+        """
+        calculate total price of the coffee, after adding sugar
+        """
         return self._coffee.price() + 0.5
     
 
 
+#basic black coffee
 coffee = Coffee()
 print("coffee price :", coffee.price())
 
-cappuccino = Cappuccino(coffee)
+#add milk, make cappuccino
+cappuccino = MilkDecorator(coffee)
 print("cappuccino price :", cappuccino.price())
 
-espresso = Espresso(coffee)
-print("espresso price :", espresso.price())
+#add sugar
+cappuccion = SugarDecorator(coffee)
+print("cappuccino with sugar price :", cappuccion.price())
