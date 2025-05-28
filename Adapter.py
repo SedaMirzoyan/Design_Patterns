5dee47e223d191bbd159a0a851a5548e13e6642e
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-29 01:05:48 +0400

    Add files via upload

--- Adapter.py
+++ Adapter.py
@@ -1,28 +1,53 @@
-#Adapter
-
-'''
-OldSystem has a method "specific_request", Target and Adapter have "request" method. 
-The Adapter adapts OldSystem to work with the Target interface, 
-allowing the client to interact with OldSystem through the Adapter using the request method.
-'''
+"""
+Adapter design pattern:
+It provides a way for two incompatible interfaces work together.
+In the following example they are OldSystem and Target.
+"""
 
 class OldSystem:
+    """
+    Old system interface
+    """
     def specific_request(slef):
+        """
+        Old method
+
+        Return:
+            string: response from the old system
+        """
         return "Specific request"
     
+
 class Target:
+    """
+    The new system interface
+    """
     def request(slef):
+        """
+        New method
+
+        Return:
+            string: response from the new system
+        """
         return "Target request"
     
 
 class Adapter(Target):
+    """
+    With the help of Adapter OldSystem and Target can work together
+    """
     def __init__(self, old_system):
         self.old_system = old_system
 
     def request(self):
+        """
+        Translates old request to the new request
+        """
         return self.old_system.specific_request()
     
 
+
+#using Target interface, but gets old system's response
 old_system = OldSystem()
 adapter = Adapter(old_system)
 
