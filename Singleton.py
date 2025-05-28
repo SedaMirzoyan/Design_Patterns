c1036c51942df7778aead49ba0929539c983e53c
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-29 00:54:30 +0400

    Add files via upload

--- Singleton.py
+++ Singleton.py
@@ -1,16 +1,45 @@
-#Singleton
+"""
+Singleton design pattern:
+Pattern makes sure only one object of a class is created, 
+and everyone uses the same instance.
+"""
 
 class Singleton:
+    """
+    Makes sure only one instance is created
+    """
+
+    #class variable, for holding one instance
     _instance = None
 
     def __new__(class_obj_itself):
+        """
+        Creates new instance or returns an existing one
+
+        Return:
+            the single instance of class
+        """
         if class_obj_itself._instance is None:
+            #if no instance exists cretae and store it
             class_obj_itself._instance = super().__new__(class_obj_itself)
             print("Creating new instance")
-
+        
+        #returns instance of the class
         return class_obj_itself._instance
     
 
+    def __init__(self):
+        """
+        Returned instance is passed for initialization
+
+        Attribute:
+            value (int): just to show singleton instance can hold data
+        """
+        self.value = None
+    
+
+
+#check if 2 objects are the same instance or not
 ob1 = Singleton()
 ob2 = Singleton()
 
