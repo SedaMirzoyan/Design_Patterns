f9eae3cd977a643ea37ef5cd150230718fd022df
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-29 00:34:43 +0400

    Add files via upload

--- Factory.py
+++ Factory.py
@@ -1,15 +1,61 @@
-#Factory
+"""
+Factory design pattern:
+It provides a special class create objects based on input,
+we don't need to know how they are built
+"""
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    Abstract base class for all animals
+    """
+    @abstractmethod
+    def speak(self):
+        pass
+
 
 class Dog:
+    """
+    Implementation of a dog
+    """
     def speak(self):
+        """
+        Returns the sound dog makes
+
+        Return:
+            string: Generic message
+        """
         return "I am dog"
 
+
 class Cat:
+    """
+    Implementation of a cat
+    """
     def speak(self):
+        """
+        Returns the sound cat makes
+
+        Return:
+            string: Generic message
+        """
         return "I am cat"
 
+
 class AnimalFactory:
+    """
+    Factory class to create animal objects
+    """
     def create_animal(self, animal):
+        """
+        create an animal object based on the provided type
+
+        Args:
+            animal (string): Type of an animal ("cat"/"dog")
+
+        Return:
+            An object of a cat or dog
+        """
         if(animal == "dog"):
             return Dog()
         elif(animal == "cat"):
@@ -17,6 +63,7 @@ class AnimalFactory:
         else:
             return None
         
+
 
 factory = AnimalFactory()
 dog = factory.create_animal("dog")
