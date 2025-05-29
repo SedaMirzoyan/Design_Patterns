ff15dc15b422ba8db41b01831d37faf5a6800af1
SedaMirzoyan <135509078+SedaMirzoyan@users.noreply.github.com>
2025-05-29 16:02:02 +0400

    Add files via upload

--- Strategy.py
+++ Strategy.py
@@ -1,15 +1,36 @@
-#Strategy
+"""
+Strategy Design pattern:
+It lets you change how something is done without changing the thing that does it.
+If we want to sort a list, instead of hardcoding bubble sort, quick sort,
+we can choose the sorting method at runtime.
+"""
 
 from abc import ABC, abstractmethod
 
 class SortingStrategy(ABC):
+    """
+    Abstract base class with sorting strategies
+    """
     @abstractmethod
     def sort(self, data):
         pass
 
 
 class BubbleSortStrategy(SortingStrategy):
+    """
+    Strategy for sorting using bubble sort algorithm
+    """
     def sort(self, data):
+        """
+        Sorts the list using bubble sort
+
+        Args:
+            data (list): list to sort
+
+        Return:
+            list:
+                The sorted list using bubble sort
+        """
         sorted = False
         for i in range(0, len(data)):
             sorted = False
@@ -25,17 +46,44 @@ class BubbleSortStrategy(SortingStrategy):
     
 
 class Sorter:
+    """
+    Class which uses sorting strategy
+
+    Attributes:
+        strategy (BubbleSortStrategy instance): The sorting strategy
+    """
     def __init__(self, strategy):
+        """
+        Initialize with a sorting strategy
+
+        Args:
+            strategy (BubbleSortStrategy instance): New sorting strategy
+        """
         self.strategy = strategy
 
+
     def sort_data(self, data):
+        """
+        Sort the data using the current strategy
+
+        Args:
+            data (list): list to sort
+
+        Return:
+            list: Sorted list
+        """
         return self.strategy.sort(data)
     
 
 
+#original unsorted list
 input = [1, -2, 0, 4, 8, -5]
+
+#create sorter with Bubble sort strategy
 bubble_sort_strategy = BubbleSortStrategy()
 sorter = Sorter(bubble_sort_strategy)
+
+#sort data using the current strategy (Bubble sort)
 sorted_data = sorter.sort_data(input)
 print(sorted_data)
 
